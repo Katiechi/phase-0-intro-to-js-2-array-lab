7810526a089875a039d0f3d1286aa8468021cf72
brian ambundo <brian.katiechi9@gmail.com>
2022-06-02 16:15:41 +0300

    commit tree f37494c6e7746d0d3641328f39a76bbbeb14f09e

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+cats.push()
+console.log(cats)
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+destructivelyAppendCat("Kanyau")
+console.log(cats)
+
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+destructivelyPrependCat("Bob")
+console.log(cats)
+
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+destructivelyRemoveLastCat()
+console.log(cats)
+
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+destructivelyRemoveFirstCat()
+console.log(cats)
+
+
+function appendCat(name){
+    return [...cats.slice(0,3),name]
+    
+}
+console.log(appendCat("Broom"))
+
+function prependCat(name){
+    return [name,...cats.slice(0,3)]
+}
+console.log(prependCat("Arnold"))
+
+
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
+console.log(removeLastCat())
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
